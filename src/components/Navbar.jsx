699686c0959a42a3ee702f46202853ade184c238
karthickkumarsm <sivakumarkarthickkumar@gmail.com>
2024-02-23 20:04:34 +0530

    resume  updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -85,7 +85,7 @@ const Navbar = () => {
                     </a>
                 </li>
                 <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f65]'>
-                    <a href='https://www.dropbox.com/scl/fi/58unahe05xvexkjdhwvp8/Karthick_Kumar_SM_Resume.pdf?rlkey=3d3nh0ll7blhoe4ioj0vwqjhr&dl=0' target="_blank" className='flex justify-between items-center w-full text-gray-300'>
+                    <a href='https://www.dropbox.com/scl/fi/1a0hj5tm38kfy18swv63k/Karthick_Kumar_SM_Resume.pdf?rlkey=ux1n0g4je1x9pm4ht9tdjv6jm&dl=0' target="_blank" className='flex justify-between items-center w-full text-gray-300'>
                         Resume <BsFillPersonLinesFill size={30}/>
                     </a>
                 </li>
