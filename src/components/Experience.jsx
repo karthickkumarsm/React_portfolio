9897a263d568d35ff2ead19f8e863e624747bda4
karthickkumarsm <sivakumarkarthickkumar@gmail.com>
2024-05-06 20:50:21 +0530

    updated experience

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -15,35 +15,32 @@ const Experience = () => {
                 <div className='max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4'>
                     <div>
                         <p className='sm:Text-right text-4xl font-bold pb-3'> 
-                            The Sparks Foundation<br/>
-                            <div className='pb-2 pt-2 text-blue-300'>Web development Intern</div>
-                           (Remote)<br/>Sept-2023
+                            GirlScript Summer of Code(GSSoC)<br/>
+                            <div className='pb-2 pt-2 text-blue-300'>Open Source Contributor</div>
+                           (Remote)<br/>May 2024 - Present
                         </p>
-                    </div>
-                    <div>
-                    • Developed a comprehensive Bank Management System during web development internship. Utilized HTML for
-                        structuring, CSS for styling, and JavaScript for dynamic functionality.<br/>
-                    • Designed an intuitive user interface ensuring seamless navigation along with implementation of responsive design
-                        for optimal user experience across devices.<br/>
-                    • Integrated client-side scripting to enhance user interaction and data manipulation.Refining skills in HTML, CSS, and JavaScript.
                     </div>
                     <div>
                         <p className='sm:Text-right text-4xl font-bold pb-3'> 
-                            Code Alpha<br/>
-                            <div className='pb-2 pt-2 text-blue-300'>Web development Intern</div>
-                           (Remote)<br/>Aug-2023
+                            Visanka Technologies Pvt Ltd<br/>
+                            <div className='pb-2 pt-2 text-blue-300'>Flutter developer Intern</div>
+                           (Remote)<br/>Feb-2024
                         </p>
                     </div>
                     <div>
-                    • Developed a dynamic multi-page form for web development internship Utilized HTML for structure, CSS for styling,
-                    and JavaScript for enhanced interactivity Implemented PHP for server-side functionality, ensuring seamless data
-                    processing.<br/>
-                    • Integrated a secure captcha system using HTML, CSS, and JavaScript to enhance form security.<br/>
-                    • Gained hands-on experience in full-stack development, showcasing proficiency in front-end and back-end
-                        technologies.<br/>
-                        <br/>
-                    </div>  
-                    
+                        <p className='sm:Text-right text-4xl font-bold pb-3'> 
+                            The Sparks Foundation<br/>
+                            <div className='pb-2 pt-2 text-blue-300'>Web developer Intern</div>
+                           (Remote)<br/>Sept-2023
+                        </p>
+                    </div> 
+                    <div>
+                        <p className='sm:Text-right text-4xl font-bold pb-3'> 
+                            Code Alpha<br/>
+                            <div className='pb-2 pt-2 text-blue-300'>Web developer Intern</div>
+                           (Remote)<br/>Aug-2023
+                        </p>
+                    </div>
                 </div>
             </div>
         </div>
